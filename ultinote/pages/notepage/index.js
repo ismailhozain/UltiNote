bd9b1761ad221afe205954efe763b789672beb8c
Ismail Hozain <ismailhozain@gmail.com>
2020-07-01 20:31:18 -0500

    babel + typescript setup.

--- ultinote/pages/notepage/index.js
+++ ultinote/pages/notepage/index.js
@@ -1,171 +1,190 @@
 import Head from 'next/head'
+import App from 'next/app'
 import {Form} from 'antd'
+import {Component} from 'react';
+import 'antd/dist/antd.css';
 
-export default function Home() {
-    return (
-        <div className="container">
-        <Head>
-        <title>UltiNote</title>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <main>
-            <div>
-                {/*ZE NAVBAR*/}
-            </div>
-            <div>
-                <Form>
-                   <Form.Item>
-                   text
-                   </Form.Item>
-                </Form>
-            </div>
-        </main>
+class Notes extends Component {
+    constructor(props) {
+        super(props)
+    }
+    componentDidMount() {
 
-        <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+    }
+    render() {
 
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
+    return(
+    <div className="container">
+            <Head>
+            <title>UltiNote</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>
+                <div>
+                    {/*ZE NAVBAR*/}
+                </div>
+                <div>
+                    <Form>
+                       <Form.Item
+                       label= {"notes:"}
+                       name={"awe"}
+                       >
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+                       </Form.Item>
+                    </Form>
+                </div>
+            </main>
 
-        footer img {
-          margin-left: 0.5rem;
-        }
+            <style jsx>{`
+            .container {
+              min-height: 100vh;
+              padding: 0 0.5rem;
+              display: flex;
+              flex-direction: column;
+              justify-content: center;
+              align-items: center;
+            }
 
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+            main {
+              padding: 5rem 0;
+              flex: 1;
+              display: flex;
+              flex-direction: column;
+              justify-content: center;
+              align-items: center;
+            }
 
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
+            footer {
+              width: 100%;
+              height: 100px;
+              border-top: 1px solid #eaeaea;
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            }
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
+            footer img {
+              margin-left: 0.5rem;
+            }
 
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
+            footer a {
+              display: flex;
+              justify-content: center;
+              align-items: center;
+            }
 
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
+            a {
+              color: inherit;
+              text-decoration: none;
+            }
 
-        .title,
-        .description {
-          text-align: center;
-        }
+            .title a {
+              color: #0070f3;
+              text-decoration: none;
+            }
 
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
+            .title a:hover,
+            .title a:focus,
+            .title a:active {
+              text-decoration: underline;
+            }
 
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
+            .title {
+              margin: 0;
+              line-height: 1.15;
+              font-size: 4rem;
+            }
 
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
+            .title,
+            .description {
+              text-align: center;
+            }
 
-          max-width: 800px;
-          margin-top: 3rem;
-        }
+            .description {
+              line-height: 1.5;
+              font-size: 1.5rem;
+            }
 
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
+            code {
+              background: #fafafa;
+              border-radius: 5px;
+              padding: 0.75rem;
+              font-size: 1.1rem;
+              font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+                DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
+            }
 
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
+            .grid {
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              flex-wrap: wrap;
 
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
+              max-width: 800px;
+              margin-top: 3rem;
+            }
 
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
+            .card {
+              margin: 1rem;
+              flex-basis: 45%;
+              padding: 1.5rem;
+              text-align: left;
+              color: inherit;
+              text-decoration: none;
+              border: 1px solid #eaeaea;
+              border-radius: 10px;
+              transition: color 0.15s ease, border-color 0.15s ease;
+            }
 
-        .logo {
-          height: 1em;
-        }
+            .card:hover,
+            .card:focus,
+            .card:active {
+              color: #0070f3;
+              border-color: #0070f3;
+            }
 
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
+            .card h3 {
+              margin: 0 0 1rem 0;
+              font-size: 1.5rem;
+            }
 
-    <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
+            .card p {
+              margin: 0;
+              font-size: 1.25rem;
+              line-height: 1.5;
+            }
 
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-)
+            .logo {
+              height: 1em;
+            }
+
+            @media (max-width: 600px) {
+              .grid {
+                width: 100%;
+                flex-direction: column;
+              }
+            }
+          `}</style>
+
+        <style jsx global>{`
+            html,
+            body {
+              padding: 0;
+              margin: 0;
+              font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+                Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+                sans-serif;
+            }
+
+            * {
+              box-sizing: border-box;
+            }
+          `}</style>
+        </div>
+    )
+
+    }
 }
+
+
+export default Notes;
